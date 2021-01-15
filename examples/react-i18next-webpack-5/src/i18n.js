afef331faf3ab7028086d10b4f615185bbb8a4ab
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2021-01-15 19:17:05 +0200

    Add webpack 5 support, closes #26

---
+++ examples/react-i18next-webpack-5/src/i18n.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  // load translation using xhr -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-xhr-backend
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    fallbackLng: 'en',
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  const { applyClientHMR } = require('i18next-hmr/client');
+  applyClientHMR(i18n);
+}
+
+export default i18n;
