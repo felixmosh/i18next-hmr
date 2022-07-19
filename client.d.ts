574deabdd45d129ec76858cd443e9da16b46401d
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2022-07-19 20:20:50 +0300

    feat: add the ability to pass a getter for i18n instance

--- client.d.ts
+++ client.d.ts
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 
 declare const client: {
-  applyClientHMR(i18n: i18next.i18n): void;
+  applyClientHMR(i18nOrGetter: i18next.i18n | (() => i18next.i18n)): void;
 };
 
 export = client;
--- server.d.ts
+++ server.d.ts
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 
 declare const server: {
-  applyServerHMR(i18n: i18next.i18n): void;
+  applyServerHMR(i18nOrGetter: i18next.i18n | (() => i18next.i18n)): void;
 };
 
 export = server;
