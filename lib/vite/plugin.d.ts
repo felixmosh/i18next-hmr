44c3621d191425cf27bbf5d5fd640c0e0833f453
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-09-12 23:19:31 +0300

    fix: export of vite plugin

--- lib/vite/plugin.d.ts
+++ lib/vite/plugin.d.ts
@@ -1,6 +1,6 @@
 interface VitePlugin {
   name: string;
 }
-export declare function I18NextHMRPlugin(
+export declare function i18nextHMRPlugin(
   options: { localesDir: string } | { localesDirs: string[] }
 ): VitePlugin;
