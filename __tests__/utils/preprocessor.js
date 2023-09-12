fa4f69ee0c7fa0de138897738b49d39cbb2fcb22
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-09-13 00:02:28 +0300

    fix: change export to cjs

--- __tests__/utils/preprocessor.js
+++ __tests__/utils/preprocessor.js
@@ -3,7 +3,11 @@ module.exports = {
     return {
       code: src
         .replace(/module\.hot/g, 'global.mockModule.hot')
-        .replace(/import\.meta\.hot/g, 'global.mockImport.meta.hot'),
+        .replace(/import\.meta\.hot/g, 'global.mockImport.meta.hot')
+        .replace(
+          /export function applyViteClientHMR/,
+          'module.exports = function applyViteClientHMR'
+        ),
     };
   },
 };
