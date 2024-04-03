228a968effdc89d6fb424ff16a3050063e7b83ce
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2024-04-03 11:23:28 +0300

    fix: add missing apply method in types

--- lib/webpack/plugin.d.ts
+++ lib/webpack/plugin.d.ts
@@ -2,5 +2,6 @@ export declare class I18NextHMRPlugin {
   static addListener(cb: (data: { lang: string; ns: string }) => void): void;
 
   constructor(options: { localesDir: string; } | { localesDirs: string[] });
+  apply: (compiler: any) => void;
 }
 
